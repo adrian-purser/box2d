e5596e9f2d10bb501dc6087c778783a60dd155e5
Adrian Purser <ade.purser@gmail.com>
2024-04-05 00:07:56 +0100

    Reduce stack allocator stack size to fit into an STM32

--- include/box2d/b2_stack_allocator.h
+++ include/box2d/b2_stack_allocator.h
@@ -26,7 +26,7 @@
 #include "b2_api.h"
 #include "b2_settings.h"
 
-const int32 b2_stackSize = 100 * 1024;	// 100k
+const int32 b2_stackSize = 32 * 1024;	// 100k
 const int32 b2_maxStackEntries = 32;
 
 struct B2_API b2StackEntry
